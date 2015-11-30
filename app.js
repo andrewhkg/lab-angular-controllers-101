25f0af5245bc16a2d350ff6cfc581e966e5fed8d
AA <andrewhkg@hotmail.com>
2015-11-30 15:35:38 +0800

    all controllers work, need optimizatin as ng-repeat

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var app = angular.module('myMuseumApp', []);
 
 app.controller('ExhibitController', function($scope){
-  $scope.image = '<img width="550" alt="picture" src="exhibits.jpg">';
+  $scope.image = 'exhibits.jpg';
   $scope.exhibits = 'Life and Theatre';
   $scope.name = 'Alexander Tikhonov'
 
 })
 
-app.controller('InfoCotroller', function(){
-  $scope.mon = "10am - 9pm";
+app.controller('HourCotroller', function($scope){
+
+  $scope.monday = '10am - 9pm';
   $scope.tuesday = "10am - 7pm";
   $scope.wednesday = "10am - 9pm";
   $scope.thursday ="10am - 7pm";
@@ -18,6 +19,7 @@ app.controller('InfoCotroller', function(){
 
 })
 
-app.controller('InfoCotroller', function(){
+app.controller('InfoCotroller', function($scope){
+  $scope.email = 'donor@donor.com';
 
 })
